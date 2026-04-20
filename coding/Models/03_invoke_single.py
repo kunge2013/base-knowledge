8a8bc239d87a09bff0a8e28c80fe59e7692d1272
方坤 <fangk046@tydic.com>
2026-04-20 11:31:40 +0800

    目录迁移

--- coding/Models/03_invoke_single.py
+++
@@ -1,9 +0,0 @@
-"""03_invoke_single.py
-Example: Invoke model with a single message.
-"""
-
-from llm_config import default_llm
-
-# Call invoke with a single message
-response = default_llm.invoke("Why do parrots have colorful feathers?")
-print(response)
